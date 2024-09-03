757c8fa2f85bd3d836ce8292c1b79bd2777b8b34
owaisdxd <150828346+Owaisdxd@users.noreply.github.com>
2024-09-03 18:44:17 +0400

    Add files via upload

---
+++ test7.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common import NoSuchElementException
+import time
+
+driver = webdriver.Chrome()
+driver.get('')
+driver.maximize_window()
+def is_available(att,abd):
+    try:
+        driver.find_element(by=att,value=abd)
+        return True
+    except NoSuchElementException:
+        return False
+print(is_available(By.TAG_NAME,'a'))
+print(is_available(By.XPATH,'option'))
+print(is_available(By.TAG_NAME,'dev'))
